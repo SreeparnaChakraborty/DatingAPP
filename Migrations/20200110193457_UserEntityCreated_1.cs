540701cbdcb75f065e0bc6469c2532c7e1351c22
SREEPARNA CHAKRABORTY <sreeparnachakraborty29@gmail.com>
2020-01-10 23:58:47 +0530

    First Commit: 1. Created DatingApp .NETCore WebAPI Project. 2. Adding Class Library For DataAccess(CodeFirst Approch). 3. Adding Necessary Nuget Pacakge For migrations.

---
+++ Migrations/20200110193457_UserEntityCreated_1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApp.API.Migrations
+{
+    public partial class UserEntityCreated_1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DatingUsers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(nullable: true),
+                    PasswordHash = table.Column<byte[]>(nullable: true),
+                    PasswordSalt = table.Column<byte[]>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DatingUsers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DatingUsers");
+        }
+    }
+}
